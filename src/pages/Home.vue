0031ef87e0f978ae8bc7990eff3ba1fa550be916
KaSchm1 <155559885+KaSchm1@users.noreply.github.com>
2024-01-08 20:43:49 +0100

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,6 @@ import Table from "../components/Table.vue"
 
 <template>
   <div class="w-full px-5 text-center">
-    // Willkommensschild vor Tabelle integrieren
    <h1 class="text-2xl font-bold">Herzlich Willkommen auf unserer Webseite.</h1>
     <p>Wir sind eine Non-Profit-Organisation, die sich gegen den Klimawandel einsetzt. Mit einer tabellarischen Aufstellung der derzeitigen CO²-Emissionen von Unternehmen, wollen wir über die aktuelle Situation aufklären und hinweisen. Die untenstehende Tabelle verfügt über eine Sortier- und Filterfunktion.</p> 
     <div class="px-5 rounded overflow-hidden shadow-lg">
