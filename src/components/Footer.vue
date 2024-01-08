476819a3a729e551677d2e994d606c599e354ced
KaSchm1 <155559885+KaSchm1@users.noreply.github.com>
2024-01-08 20:36:17 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,11 @@
 <template>
   <footer>
     <ul class="flex flex-col lg:flex-row list-none ml-auto">
-      //Impressum importiern bei Fußzeile
       <li>
         <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="#impressum">
           <span class="ml-2">Impressum</span>
         </a>
       </li>
-      //Datenschutz importieren bei Fußzeile
       <li>
         <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="#datenschutz">
           <span class="ml-2">Datenschutz</span>
