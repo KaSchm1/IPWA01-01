8c3890aaf3229bc6367a18f6b2205cefbecc5e11
KaSchm1 <155559885+KaSchm1@users.noreply.github.com>
2024-01-08 21:45:38 +0100

    Update Table.vue

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative m-2 ">
         <input v-model="searchTerm" type="text" id="search" class="mt-4 block pb-2.5 pt-4 w-full text-sm text-green-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-green-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-        <label for="search" class="absolute text-sm text-green-500 dark:text-green-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-green-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Suchbegriff eingeben...</label>
+        <label for="search" class="absolute text-sm text-green-500 dark:text-green-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-green-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Was möchten Sie suchen?</label>
     </div>
     <table-lite class="mb-5"
       :is-static-mode="true"
