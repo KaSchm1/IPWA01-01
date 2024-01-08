4bc596deaaf47dc27120ca4aafdc7c4147ad01c7
KaSchm1 <155559885+KaSchm1@users.noreply.github.com>
2024-01-08 20:36:55 +0100

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,4 @@
 <template>
-   //Logo einfügen
     <nav :dir="langDirection" class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-200 mb-3">
       <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div class="w-full relative flex justify-between lg:w-auto  px-2 lg:static lg:block lg:justify-start">
