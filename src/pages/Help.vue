057cc97f9baeaebec0b13984a8b63bec06413080
KaSchm1 <155559885+KaSchm1@users.noreply.github.com>
2024-01-08 20:22:49 +0100

    Update Help.vue

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -13,7 +13,7 @@
                   class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white"
                 >
                   <svg
-                    class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                    class="flex-shrink-0 mr-2 w-5 h-5 text-green-500 dark:text-green-400"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -24,12 +24,10 @@
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  Was ist CO2?
+                  Was bedeutet "Klima"?
                 </h3>
-                <p class="text-gray-500 dark:text-gray-400">
-                  Kohlendioxid ist eine chemische Verbindung aus Kohlenstoff und
-                  Sauerstoff. CO₂ ist ein nicht brennbares, saures und farbloses
-                  Gas.
+                <p class="text-black-500 dark:text-black-400">
+                 Das Wort "Klima" bezieht sich normalerweise auf das langfristige durchschnittliche Wetter in einem bestimmten Gebiet. Es umfasst Parameter wie Temperatur, Luftfeuchtigkeit, Niederschlag, Windgeschwindigkeit und andere meteorologische Faktoren. Das Klima eines Ortes wird durch verschiedene Faktoren beeinflusst, darunter geografische Lage, Höhe, Meeresströmungen und Vegetation. Darüber hinaus wird der Begriff "Klima" auch in übertragenem Sinne verwendet, um die allgemeine Atmosphäre oder Stimmung in einer Gruppe oder einem Umfeld zu beschreiben. Zum Beispiel kann man von einem "Arbeitsklima" sprechen, um die allgemeine Atmosphäre und Beziehungen zwischen den Mitarbeitern in einem Unternehmen zu beschreiben.
                 </p>
               </div>
               <div class="mb-10">
@@ -48,11 +46,10 @@
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  Wer sind wir?
+                  Was sind die Ursachen von Klimaänderungen?
                 </h3>
-                <p class="text-gray-500 dark:text-gray-400">
-                  Wir sind eine gemeinnützige Organisation mit dem Ziel die
-                  Bevölkerung auf den CO2 Fußabdruck aufmerksam zu machen.
+                <p class="text-black-500 dark:text-black-400">
+                  1. Natürliche Faktoren: Solare Variabilität: Änderungen in der Aktivität der Sonne können direkte Auswirkungen auf das Klima der Erde haben. Vulkanausbrüche: Vulkanische Aktivität kann Partikel und Gase in die Atmosphäre freisetzen, die das Klima beeinflussen können, indem sie Sonnenlicht reflektieren und die Temperaturen beeinflussen. 2. Menschliche Faktoren (anthropogene Ursachen): Treibhausgasemissionen: Die Verbrennung fossiler Brennstoffe wie Kohle, Öl und Gas führt zu einem Anstieg von Treibhausgasen wie Kohlendioxid (CO2), Methan (CH4) und Lachgas (N2O) in der Atmosphäre. Diese Gase verstärken den natürlichen Treibhauseffekt, was zu einer Erwärmung der Erde führt. Entwaldung: Die Abholzung von Wäldern reduziert die Fähigkeit der Erde, CO2 zu absorbieren, und trägt somit zur Anhäufung von Treibhausgasen in der Atmosphäre bei. Landnutzungsänderungen: Veränderungen in der Art und Weise, wie Menschen das Land nutzen, können das Klima beeinflussen, indem sie die Oberflächenalbedo, den Wasserkreislauf und andere klimarelevante Prozesse verändern. 3. Naturale interne Variabilität: Ozeanische Zyklen: Langfristige zyklische Veränderungen in den Ozeanen, wie zum Beispiel das Phänomen El Niño und La Niña, können das Klima beeinflussen. Ozeanische Zirkulation: Änderungen in den Meeresströmungen können lokale und regionale Klimabedingungen beeinflussen.
                 </p>
               </div>
             </div>
@@ -73,11 +70,10 @@
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  Was machen wir?
+                  Wie können wir den CO²-Ausstoß verringern?
                 </h3>
-                <p class="text-gray-500 dark:text-gray-400">
-                  Wir stellen eine Datenbank mit CO2 Werten bereit, welche sich
-                  nach Land und Unternehmen filtern und sortieren lässt.
+                <p class="text-black-500 dark:text-black-400">
+                  Umstellung auf erneuerbare Energien: Investitionen in erneuerbare Energiequellen wie Solarenergie, Windenergie, Wasserkraft und Geothermie können den CO2-Ausstoß aus der Energieerzeugung erheblich reduzieren. Energieeffizienz: Verbesserung der Energieeffizienz in Gebäuden, Industrie und Verkehr, um den Energieverbrauch zu minimieren und den CO2-Ausstoß pro Einheit Energie zu verringern. Elektrifizierung des Verkehrs: Förderung von Elektrofahrzeugen und die Entwicklung einer Infrastruktur für Ladestationen können den CO2-Ausstoß im Verkehrssektor reduzieren, insbesondere wenn der Strom aus erneuerbaren Quellen stammt. Aufforstung und Waldschutz: Pflanzung von Bäumen und Schutz bestehender Wälder, da Bäume CO2 absorbieren und speichern können. Nachhaltige Landwirtschaft: Förderung von Praktiken in der Landwirtschaft, die den CO2-Ausstoß minimieren, wie z.B. nachhaltige Bewirtschaftung, Fruchtfolge, und Reduzierung von Stickstoffdünger.
                 </p>
               </div>
               <div class="mb-10">
@@ -96,10 +92,10 @@
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  Kann ich unterstützen?
+                  Was ist der Unterschied zwischen Wetter und Klima?
                 </h3>
-                <p class="text-gray-500 dark:text-gray-400">
-                  Ja, mit einer kleinen Spende kannst du dich beteiligen.
+                <p class="text-black-500 dark:text-black-400">
+                  Das Wetter bezieht sich auf den kurzfristigen Zustand der Atmosphäre an einem bestimmten Ort und zu einem bestimmten Zeitpunkt. Es umfasst kurzfristige atmosphärische Phänomene wie Temperatur, Luftfeuchtigkeit, Niederschlag, Windgeschwindigkeit, Bewölkung und Luftdruck. Das Wetter ändert sich ständig und kann von Tag zu Tag, von Stunde zu Stunde oder sogar von Minute zu Minute variieren. Klima: Das Klima hingegen beschreibt das langfristige Durchschnittswetter in einem bestimmten Gebiet über einen längeren Zeitraum, typischerweise über 30 Jahre oder mehr. Es berücksichtigt statistische Informationen über Temperatur, Niederschlag, Wind und andere Wetterelemente, um charakteristische Muster und Trends zu identifizieren. Das Klima bietet eine stabile Grundlage für die Erwartung der Wetterbedingungen in einer bestimmten Region zu verschiedenen Jahreszeiten.
                 </p>
               </div>
             </div>
