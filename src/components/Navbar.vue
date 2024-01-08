a056168cf441263623f6c2ae7034ab61c5bbfdda
KaSchm1 <155559885+KaSchm1@users.noreply.github.com>
2024-01-08 20:49:08 +0100

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,13 +18,11 @@
                   <span class="ml-2">Hilfe</span>
               </a>
             </li>
-            //Impressum zu Menü hinzufügen
               <li class="nav-item">
               <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="#impressum">
                 <span class="ml-2">Impressum</span>
               </a>
             </li>
-              //Datenschutz hinzufügen
             <li class="nav-item">
               <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="#datenschutz">
                 <span class="ml-2">Datenschutz</span>
