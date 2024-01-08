c49db20ba9853d7132ad2d7a38c40f6efe0a87bf
KaSchm1 <155559885+KaSchm1@users.noreply.github.com>
2024-01-08 20:37:41 +0100

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav :dir="langDirection" class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-200 mb-3">
+    <nav :dir="langDirection" class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-green-200 mb-3">
       <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div class="w-full relative flex justify-between lg:w-auto  px-2 lg:static lg:block lg:justify-start">
           <a class="text-sm font-bold leading-relaxed inline-block mr-4 whitespace-nowrap uppercase" href="#">
